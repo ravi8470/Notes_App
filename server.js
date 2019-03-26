5e53b9c3f3d5e78100d92ccb8dcc7a48871e1eb9
ravi8470 <rvkmr0851@gmail.com>
2019-03-27 02:01:05 +0530

    added bulma css templates

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+//Success values --
+// 5 indicates dummy values to escape success is not defined errors.
+
+
 var express = require('express');
 const bodyparser = require("body-parser");
 var mysql = require('mysql');
@@ -6,7 +10,7 @@ const bcrypt = require('bcrypt');
 //var ejs = require('ejs');
 var conn = mysql.createConnection({
   host: "127.0.0.1",
-  user: "root",
+  user: "newuser",
   password: "ravi",
   database: "todo_app_db"
 });
@@ -17,250 +21,251 @@ app.use(bodyparser.urlencoded({
   extended: true
 }));
 app.set('view engine', 'ejs');
-app.use(session({secret: "ravi",resave: false, saveUninitialized: false}));
-conn.connect(function(err){
-  if(err) throw err;})
+app.use(session({ secret: "ravi", resave: false, saveUninitialized: false }));
+conn.connect(function (err) {
+  if (err) throw err;
+})
 app.post("/adduser", (req, resp) => {
   var numOfRecords;
   var user = req.body.uname;
   var pass = req.body.upass;
-  var sql2 = "SELECT COUNT(username) from logins WHERE username = '"+ user + "'";
+  var sql2 = "SELECT COUNT(username) from logins WHERE username = '" + user + "'";
   console.log(sql2);
-  conn.query(sql2, (err, res)=>{
-    if(err) throw err;
-    if(res[0]['COUNT(username)'] == 0){
+  conn.query(sql2, (err, res) => {
+    if (err) throw err;
+    if (res[0]['COUNT(username)'] == 0) {
       let hash = bcrypt.hashSync(pass, 10);
-      var sql = "INSERT INTO logins(username, password) VALUES ('" + user + "','" + hash +"')";
+      var sql = "INSERT INTO logins(username, password) VALUES ('" + user + "','" + hash + "')";
       console.log(sql);
-      conn.query(sql, function(err,res){
-        if(err) throw err;
+      conn.query(sql, function (err, res) {
+        if (err) throw err;
         //numOfRecords = res.length;
         console.log("New User inserted ", user);
-        resp.render('index',{success: 111});
-  })
+        resp.render('index', { success: 111 });
+      })
     }
-    else{
-      resp.render('index',{success: 200})
+    else {
+      resp.render('index', { success: 200 })
     }
-    
+
   })
 });
 var numRec;
-app.post("/login",(reqp, resp) => {
-  if(reqp.session.ID && reqp.session.username){
-    resp.render('dashboard',{name: reqp.session.username})
+app.post("/login", (reqp, resp) => {
+  if (reqp.session.ID && reqp.session.username) {
+    resp.render('dashboard', { name: reqp.session.username, success: 5 })
   }
-  else{
+  else {
     var user = reqp.body.uname;
     var pass = reqp.body.upass;
     let hash = bcrypt.hashSync(pass, 10);
     //console.log("hashed pass is: ", hash);
-    var sql = "SELECT ID, username, password FROM logins WHERE username = '"+user + "'";
+    var sql = "SELECT ID, username, password FROM logins WHERE username = '" + user + "'";
     console.log(sql);
-    conn.query(sql, function(err,res){
-      if(err) throw err;
+    conn.query(sql, function (err, res) {
+      if (err) throw err;
       numRec = res.length;
       console.log("Num of records found: ", res.length);
-      if(res.length == 1){
+      if (res.length == 1) {
         //console.log("found a user.. matching pass next");
         //console.log("hash is :" + hash + "and pass is " + res[0]['password']);
-        if(bcrypt.compareSync(reqp.body.upass, res[0]['password'])){
+        if (bcrypt.compareSync(reqp.body.upass, res[0]['password'])) {
           //console.log("password comparison succeeded");
           reqp.session.ID = res[0]['ID'];
           reqp.session.username = res[0]['username']
           console.log("saving the ID session variable with value: ", reqp.session.ID);
           //selectRecentNotes(reqp, resp);
-          resp.render('dashboard',{name: reqp.session.username});
+          resp.render('dashboard', { name: reqp.session.username, success: 5 });
         }
-        else{
-          resp.render('index', {success: 666});
+        else {
+          resp.render('index', { success: 666 });
         }
       }
-      else{
-        resp.render('index',{success: 666})
+      else {
+        resp.render('index', { success: 666 })
       }
     })
-  }  
-});
-app.get('/login', (req,res) => {
-  if(req.session.ID && req.session.username){
-    res.render('dashboard',{name: req.session.username});
   }
-  else{
-    res.render('index',{success: 0});
+});
+app.get('/login', (req, res) => {
+  if (req.session.ID && req.session.username) {
+    res.render('dashboard', { name: req.session.username, success: 5 });
+  }
+  else {
+    res.render('index', { success: 0 });
   }
 });
 
 //Saving Notes
-app.post("/saveNote",(reqp,resp) => {
-  if(typeof reqp.session.ID != "undefined"){
+app.post("/saveNote", (reqp, resp) => {
+  if (typeof reqp.session.ID != "undefined") {
     console.log(reqp.session.ID);
     var sql = "INSERT INTO notes(ID, title, created, note_text) VALUES (" +
-    reqp.session.ID + ",'" + reqp.body.noteTitle + "', NOW(),'"+ reqp.body.noteBody + "')";
+      reqp.session.ID + ",'" + reqp.body.noteTitle + "', NOW(),'" + reqp.body.noteBody + "')";
     console.log(sql);
-    conn.query(sql, function(err, res){
-      if(err) throw err;
+    conn.query(sql, function (err, res) {
+      if (err) throw err;
       // selectRecentNotes(reqp,resp);
-      resp.render('dashboard',{name: reqp.session.username});
+      resp.render('dashboard', { name: reqp.session.username, success: 288 });
     })
   }
-  else{
-    resp.render('index',{success: 333}) 
+  else {
+    resp.render('index', { success: 299 })
   }
 });
 
 //Checking for Unique username through AJAX Call
 app.post("/checkUnique", (req, res) => {
   console.log(req.body.uname);
-  var sql2 = "SELECT COUNT(username) from logins WHERE username = '"+ req.body.uname + "'";
+  var sql2 = "SELECT COUNT(username) from logins WHERE username = '" + req.body.uname + "'";
   console.log(sql2);
-  conn.query(sql2, (err, resp)=>{
-    if(err) throw err;
-    if(resp[0]['COUNT(username)'] != 0){
+  conn.query(sql2, (err, resp) => {
+    if (err) throw err;
+    if (resp[0]['COUNT(username)'] != 0) {
       res.set('statusCode', '200');
       res.set('Content-Type', 'application/json');
-      res.send({status: 0, text:"<font color='red'>User already exists!</font>"});
+      res.send({ status: 0, text: "<font color='red'>User already exists!</font>" });
     }
-    else{
+    else {
       res.set('statusCode', '200');
       res.set('Content-Type', 'application/json');
-      res.send({status: 1, text:""});
-    }  
+      res.send({ status: 1, text: "" });
+    }
   })
-  
+
 });
 
-app.get("/adduser", (req, res)=>{
+app.get("/adduser", (req, res) => {
   // res.sendFile(__dirname + "/views/adduser.html");
-  res.render('adduser',{success: 0});
+  res.render('adduser', { success: 0 });
 });
 
-app.get("/logout", (req,res) => {
-  req.session.destroy(function(err){
-    if(err) throw err;
+app.get("/logout", (req, res) => {
+  req.session.destroy(function (err) {
+    if (err) throw err;
   });
-  res.render('index',{success: 0});
+  res.render('index', { success: 0 });
 });
 
 
-app.post("/fetchNotes", (req,res) => {
-  if(req.session.ID){
+app.post("/fetchNotes", (req, res) => {
+  if (req.session.ID) {
     console.log("the offset received was ", req.body);
-    var sql = "SELECT COUNT(note_id) from notes WHERE ID = " + req.session.ID ;
-    conn.query(sql,(err, resp) => {
+    var sql = "SELECT COUNT(note_id) from notes WHERE ID = " + req.session.ID;
+    conn.query(sql, (err, resp) => {
       console.log(resp);
       req.session.numNotes = resp[0]['COUNT(note_id)'];
       console.log("num notes stored are ", req.session.numNotes);
       req.session.numPages = Math.ceil(req.session.numNotes / 4);
       console.log("Number of pages are ", req.session.numPages);
       console.log("request body is", req.body);
-      var sql2 = "SELECT * from notes WHERE ID = " + req.session.ID + " LIMIT 4 OFFSET " + ((req.body.offset-1)*4);
+      var sql2 = "SELECT * from notes WHERE ID = " + req.session.ID + " LIMIT 4 OFFSET " + ((req.body.offset - 1) * 4);
       console.log(sql2);
       conn.query(sql2, (err, ress) => {
         console.log(ress);
         console.log("Num of things retrieved ", ress.length);
         res.set('statusCode', '200');
         res.set('Content-Type', 'application/json');
-        res.send({payload:ress,numPages: req.session.numPages});
-      })      
+        res.send({ payload: ress, numPages: req.session.numPages });
+      })
     });
   }
-  else{
-    res.render('index',{success: 0});
+  else {
+    res.render('index', { success: 0 });
   }
 });
 
 app.post("/updateNote", (reqp, resp) => {
   console.log("Data received is :", reqp.body.note_id, reqp.body.noteTitle, reqp.body.noteBody);
   var sql = "UPDATE notes SET title = '" + reqp.body.noteTitle + "' ,note_text = '" + reqp.body.noteBody +
-   "' ,created = NOW() WHERE note_id = " + reqp.body.note_id;
+    "' ,created = NOW() WHERE note_id = " + reqp.body.note_id;
   console.log(sql);
   conn.query(sql, (err, res) => {
-    if(err) throw err;
+    if (err) throw err;
     console.log(res);
     //Maybe async await here so that after the update call is finished that we start the retrieval process so the
     // updated note also gets retrieved.
     // selectRecentNotes(reqp,resp);
-    if(res.changedRows == 1 || res.affectedRows == 1){
-      resp.render('dashboard',{name: reqp.session.username, noteEdited: 55});
+    if (res.changedRows == 1 || res.affectedRows == 1) {
+      resp.render('dashboard', { name: reqp.session.username, noteEdited: 55, success: 5 });
     }
-    else{
-      resp.render('dashboard',{name: reqp.session.username, noteEdited: 66});
+    else {
+      resp.render('dashboard', { name: reqp.session.username, noteEdited: 66, success: 5 });
     }
-    
+
   })
-  
+
 });
 
 
-app.get("/deleteNote", (req,res) => {
-  console.log("Note id received for deleting is: ",req.query.note_id);
+app.get("/deleteNote", (req, res) => {
+  console.log("Note id received for deleting is: ", req.query.note_id);
   var sql = "DELETE FROM notes WHERE note_id= " + req.query.note_id;
   conn.query(sql, (err, resp) => {
-    if(err) throw err;
+    if (err) throw err;
     console.log(resp);
     res.set('statusCode', '200');
     res.set('Content-Type', 'text/html');
-    if(resp.affectedRows == 1){
+    if (resp.affectedRows == 1) {
       res.send("<font color='green'>Note was deleted</font>");
     }
-    else{
+    else {
       res.send("<font color='red'>Error in deleting note.</font>");
     }
   })
-  
+
 });
 
-app.get("/searchNotes", (req,resp) => {
-  console.log(req.session.ID,req.session.username);
-  if(req.session.ID && req.session.username){
-    console.log("Value received to search is: ",req.query.searchTerm);
+app.get("/searchNotes", (req, resp) => {
+  console.log(req.session.ID, req.session.username);
+  if (req.session.ID && req.session.username) {
+    console.log("Value received to search is: ", req.query.searchTerm);
     var sql = "SELECT * FROM notes WHERE title LIKE '%" + req.query.searchTerm + "%' OR note_text LIKE '%" + req.query.searchTerm + "%'";
     console.log(sql);
-    conn.query(sql, (err, res) =>{
-      if(err) throw err;
+    conn.query(sql, (err, res) => {
+      if (err) throw err;
       console.log(res);
       resp.set('statusCode', '200');
       resp.set('Content-Type', 'application/json');
-      resp.send({searchResult: res});
+      resp.send({ searchResult: res });
     })
   }
 });
 
 app.get("/", (req, res) => {
-  console.log("entered the use/get"); 
-  if(req.session.ID > -1 && req.session.username != ""){
-    res.render('dashboard',{name: req.session.username})
+  console.log("entered the use/get");
+  if (req.session.ID > -1 && req.session.username != "") {
+    res.render('dashboard', { name: req.session.username, success: 5 })
   }
-  else{
-    res.render('index',{success: 0});
+  else {
+    res.render('index', { success: 0 });
   }
 });
 app.use(function (req, res, next) {
   res.status(404).send("<font color='red'>Sorry, This page is in another Castle!</font>")
 });
 app.listen(port, () => {
-  console.log("Server listening at port: "+ port);
+  console.log("Server listening at port: " + port);
 });
 
 
 
-function selectRecentNotes(reqp, resp){  
+function selectRecentNotes(reqp, resp) {
   console.log("function selectRecentNotes called");
-  var sql3 = "SELECT * FROM notes WHERE ID = " + reqp.session.ID +" ORDER BY created DESC LIMIT 3";
+  var sql3 = "SELECT * FROM notes WHERE ID = " + reqp.session.ID + " ORDER BY created DESC LIMIT 3";
   console.log(sql3);
-  conn.query(sql3, (errr, resqq) =>{
-    if(errr) throw errr;
+  conn.query(sql3, (errr, resqq) => {
+    if (errr) throw errr;
     console.log(resqq);
-    if(resqq == undefined){
+    if (resqq == undefined) {
       reqp.session.notesLen = 0;
       reqp.session.notes = []
-    }          
-    else{
+    }
+    else {
       reqp.session.notes = resqq;
       reqp.session.notesLen = resqq.length
     }
-    resp.render('dashboard',{name: reqp.session.username});
+    resp.render('dashboard', { name: reqp.session.username, success: 5 });
   })
 }
 // app.get('*', function(req, res){
